5918c32f783f067da81e58f54f982f00440e56e4
Anderson Barbosa Magalhães <andcnsamobile@gmail.com>
2016-04-28 20:47:34 -0300

    Commit inicial.

---
+++ src/test/java/com/bomtopado/lanchonete/LanchoneteApplicationTests.java
@@ -0,0 +1,18 @@
+package com.bomtopado.lanchonete;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = LanchoneteApplication.class)
+@WebAppConfiguration
+public class LanchoneteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
